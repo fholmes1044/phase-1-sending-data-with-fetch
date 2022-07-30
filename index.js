b8f12af0c029b26bf509f1873bfdbc9b33790b8e
+ <+>
2022-07-29 21:55:26 -0400

    commit tree 7ef19e159043fe6c548b5b734699c29a29fc0a44

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(user_name, user_email){
+     return fetch("http://localhost:3000/users",{
+      method: "POST",
+      headers:{
+
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        name: user_name,
+        email: user_email,
+      })
+    })
+   
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        console.log(object) 
+        document.body.innerHTML = object.id
+    })
+.catch(function(error){
+   
+    document.body.innerHTML = error.message
+})
+}
